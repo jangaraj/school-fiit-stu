73e2948b30138d3ea258372d1319c2bcf442c66c
jan.garaj <jan.garaj@fbb42378-df2e-0410-9258-e76f0781622f>
2007-04-25 11:16:53 +0000

    Acm minus pridany verzia s mallockom - security exception error :(

---
+++ trunk/test/acm_minus.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_LINE_LENGTH 255
+
+int main(void) 
+{
+	char vstup[MAX_LINE_LENGTH], *out;
+	int i;
+
+	i = 1;
+    if((out = (char *) malloc(sizeof(char)))==NULL) {
+         fprintf(stderr,"Error, malloc pole_start\n");
+         exit (1);
+    }
+	*out = '\0';
+	while(fgets(vstup,MAX_LINE_LENGTH,stdin)!=NULL) {
+		out = (char *) realloc(out,i+1 * sizeof(char));
+		i++;
+		if(vstup[0]=='-') {
+			strcat(out,vstup+sizeof(char));
+			strcat(out,"\n");
+			//printf("%s\n",(vstup+sizeof(char)));
+		}
+		else {
+			strcat(out,vstup);
+			strcat(out,"\n");
+//			printf("%s\n",vstup);
+		}
+	}
+	printf("%s",out);
+	return 0;
+}
